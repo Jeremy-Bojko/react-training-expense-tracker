b55fa9b5941f766de95e3db2cfc13472df704ba5
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-03-08 11:20:12 +0100

    1 - First custom component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>8 mars 2022</div>
+            <div>
+                <h2>Achat fruits et légumes 🥦 🍊</h2>
+                <div>10,50€</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
