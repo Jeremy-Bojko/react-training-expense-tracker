680bdf8b4696cb09980c7ab16391ade9122d645a
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-03-08 16:05:27 +0100

    6 - Split code into another Component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(props) {
+    const day = props.date.toLocaleDateString('fr-FR', { day: 'numeric' });
+    const month = props.date.toLocaleDateString('fr-FR', { month: 'long' });
+    const year = props.date.getFullYear();
+    return(
+        <div className='expense-date'>
+            <div className='expense-date__day'>{ day }</div>
+            <div className='expense-date__year'>{ year }</div>
+            <div className='expense-date__month'>{ month }</div>
+        </div>
+    );
+}
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const day = props.date.toLocaleDateString('fr-FR', { day: 'numeric' });
-    const month = props.date.toLocaleDateString('fr-FR', { month: 'long' });
-    const year = props.date.getFullYear();
+
     return (
         <div className="expense-item">
-            <div>
-                <div>{ day }</div>
-                <div>{ month }</div>
-                <div>{ year }</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{ props.title }</h2>
                 <div className="expense-item__price">{ props.amount.toFixed(2) }€</div>
