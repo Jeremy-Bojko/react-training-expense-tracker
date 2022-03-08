39895ce56cc980606b5609e1022abb536f51a01e
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-03-08 11:26:58 +0100

    2 - Adding Styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className="expense-item">
             <div>8 mars 2022</div>
-            <div>
+            <div className="expense-item__description">
                 <h2>Achat fruits et légumes 🥦 🍊</h2>
-                <div>10,50€</div>
+                <div className="expense-item__price">10,50€</div>
             </div>
         </div>
     )
