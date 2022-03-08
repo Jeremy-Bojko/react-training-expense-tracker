401bf88db0604cfa835905fe10c84c23de1ae63b
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-03-08 12:11:21 +0100

    5 - Add js logic into Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {    
+function ExpenseItem(props) {
+    const day = props.date.toLocaleDateString('fr-FR', { day: 'numeric' });
+    const month = props.date.toLocaleDateString('fr-FR', { month: 'long' });
+    const year = props.date.getFullYear();
     return (
         <div className="expense-item">
-            <div>{ props.date.toLocaleDateString() }</div>
+            <div>
+                <div>{ day }</div>
+                <div>{ month }</div>
+                <div>{ year }</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{ props.title }</h2>
                 <div className="expense-item__price">{ props.amount.toFixed(2) }€</div>
