7696b69bc2defb1ebab49164a11dd14a6f189bdd
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-03-08 11:40:05 +0100

    3 - Work with dynamic data in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 2, 8);
+    const expenseTitle = 'Achat fruits et légumes 🥦 🍊';
+    const expenseAmount = 10.50;
+    
     return (
         <div className="expense-item">
-            <div>8 mars 2022</div>
+            <div>{ expenseDate.toLocaleDateString() }</div>
             <div className="expense-item__description">
-                <h2>Achat fruits et légumes 🥦 🍊</h2>
-                <div className="expense-item__price">10,50€</div>
+                <h2>{ expenseTitle }</h2>
+                <div className="expense-item__price">{ expenseAmount.toFixed(2) }€</div>
             </div>
         </div>
     )
